c22c22069dec03c1fda6be6bc1f0eb3fb56798e0
Lee Salminen <leesalminen@gmail.com>
2013-12-18 21:50:21 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BtcAlerts::Application.config.secret_key_base = '79087c6333808026fe596fbb90138d49dc925c80b03fb6eaf19bc8d90240b53b31a4e78389eef5a500856762cdc57a0f30fa27ac2e8a83b835a728174627b335'
