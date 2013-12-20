9426fb7f045ff80d5f20fa29aa4cacd9a5ebe652
Lee Salminen <leesalminen@gmail.com>
2013-12-19 21:46:16 -0700

    America/Denver, not Denver for timezone???

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 BtcAlerts::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.time_zone = "Denver"
+  config.time_zone = "America/Denver"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
