6e4b4db6bda1f534af594b4bc32da80ae848ab78
Lee Salminen <leesalminen@gmail.com>
2013-12-19 22:11:21 -0700

    perhaps @current_user hasn't been defined yet (due to before_action). So, lets define it ourselves, then run the if statment to redirect if not authenticated on protected pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < ActionController::Base
   end
 
   def require_login
-    unless @current_user
+    user = User.find(session[:user_id]) if session[:user_id]
+    unless user
       redirect_to("/auth/failure", {:flash => { :error => "You must be logged in to see that page." }, :status => :moved_permanently})
     end
   end
