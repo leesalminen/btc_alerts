f1a6cbade2a0c659d2de450bee8fefb6b938841d
Lee Salminen <leesalminen@gmail.com>
2013-12-19 17:00:07 -0700

    used the wrong client ID for google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '239299412902071', '957e91b06f01f45403cdc5d21df580a6'
-  provider :google_oauth2, 'AIzaSyCqiuYlZRbGHyqTHdBZf6tID2_4BFKNu6w', 'WshAnLWrluS5A2Np74eSJtlI'
+  provider :google_oauth2, '6241257363-vssnpupnsliovsmelm74mbr8ojb08kf5.apps.googleusercontent.com', 'WshAnLWrluS5A2Np74eSJtlI'
 end
