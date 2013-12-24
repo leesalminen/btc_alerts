bbb4df91f1a1fc2513062957537797af0459ab50
Lee Salminen <leesalminen@gmail.com>
2013-12-23 18:46:55 -0700

    abstracting queries to model for Price. adding highcharts graphs to price pages.

--- app/controllers/price_controller.rb
+++ app/controllers/price_controller.rb
@@ -1,12 +1,38 @@
 class PriceController < ApplicationController
 	skip_before_action :require_login
 	def coinbase
-		@prices = Price.where(source: "coinbase").last(100).reverse
+		@prices = Price.get_coinbase()
+		
+		@data_price = []
+		@prices.each do |p|
+   			@data_price << p.rate.to_s.to_f
+		end
+    	@data_price = @data_price.join(", ")
 	end
+
 	def mtgox
-		@prices = Price.where(source: "mtgox").last(100).reverse
+		@prices = Price.get_mtgox()
+
+		@data_price = []
+		@prices.each do |p|
+   			@data_price << p.rate.to_s.to_f
+		end
+    	@data_price = @data_price.join(", ")
 	end
+
 	def all
-		@prices = Price.last(200).reverse
+		@prices = Price.get_all()
+
+		@data_price_mtgox = []
+    	@data_price_coinbase = []
+		@prices.each do |p|
+			if p.source == 'coinbase'
+   				@data_price_coinbase << p.rate.to_s.to_f
+   			elsif p.source == 'mtgox'
+   				@data_price_mtgox << p.rate.to_s.to_f
+   			end
+		end
+    	@data_price_coinbase = @data_price_coinbase.join(", ")
+    	@data_price_mtgox 	 = @data_price_mtgox.join(", ")
 	end
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,2 +1,17 @@
 class Price < ActiveRecord::Base
+  def self.get_coinbase()
+    @prices = Price.where(source: "coinbase").last(100).reverse
+  end
+
+  def self.get_mtgox()
+    @prices = Price.where(source: "mtgox").last(100).reverse
+  end
+
+  def self.get_last_200()
+    @prices = Price.last(200).reverse
+  end
+
+  def self.get_all()
+    @prices = Price.all
+  end
 end
